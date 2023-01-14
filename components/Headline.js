dc30a5ff60c43ad46dfb6fef9744c4aa9126c064
misosoup0819 <misosoup0819@gmail.com>
2023-01-14 18:51:24 +0900

    Main.jsを追加

--- components/Headline.js
+++ components/Headline.js
@@ -1,13 +1,13 @@
 import styles from "../styles/Home.module.css";
 
-export default function Headline(props) {
+export function Headline(props) {
   console.log(props);
   return (
     <div>
       <h1 className={styles.title}>{props.page} Page</h1>
       <p className={styles.description}>
         Get started by editing&nbsp;
-        <code className={styles.code}>pages/{props.page}.js</code>
+        {props.children}
       </p>
     </div>
   );
